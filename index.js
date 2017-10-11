dd579fd68559526b6f77063f68a1f5018b079cb9
Yerim Hu <yerimhu88@gmail.com>
2017-10-11 14:52:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ function divideBy3() {
 function add(a,b) {
   return a + b;
 }
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n;
+}
